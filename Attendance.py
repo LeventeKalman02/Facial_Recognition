c238a4dbaa0092a3bf0e85800659c727fe4ee3c5
EnhancedTarget <levente.kalman922@gmail.com>
2025-07-02 22:05:05 +0100

    write tasks to complete for attendance.py

--- Attendance.py
+++ Attendance.py
@@ -1,4 +1,14 @@
 import cv2
 import numpy as np
 import face_recognition
+import os
+
+# Tasks:
+# Set the path to the directory containing the images
+# Create a list to hold the images and their names
+# Generate the encodings for each image in the directory
+# Try to recognize face in the webcam feed
+
+path = 'ImagesAttendance'  
+images = []
 
